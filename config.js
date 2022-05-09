3549c573ab2244db3a10901ed10857b945c20d8c
CristoferDuarte <crismellogamer@gmail.com>
2022-05-09 16:36:31 -0300

    Insert Firebase

--- config.js
+++ config.js
@@ -1,9 +1,14 @@
 import firebase from 'firebase';
-
-// adicione SDK do Firebase
 var firebaseConfig = {
+    apiKey: "AIzaSyCJrYbqiwiuzXF7gOx1zAECuRVgZZKkoDQ",
+    authDomain: "cubinho.firebaseapp.com",
+    databaseURL: "https://cubinho-default-rtdb.firebaseio.com",
+    projectId: "cubinho",
+    storageBucket: "cubinho.appspot.com",
+    messagingSenderId: "373419264520",
+    appId: "1:373419264520:web:719a44da7f57d8d3eadfbd"
+  };  
 
-};
 // Initialice o Firebase
 firebase.initializeApp(firebaseConfig);
 
